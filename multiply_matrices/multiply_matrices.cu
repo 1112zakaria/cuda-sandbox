e64228019ceba4e8f01de95aa680e077ae2cbd51
Zakaria Ismail <1112zakaria@gmail.com>
2022-11-28 02:24:38 -0500

    **multiply_matrices**: empty placeholder
    **Description**
     - empty placeholder file, committed so that I can continue on
    other computer perhaps

---
+++ multiply_matrices/multiply_matrices.cu
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    /*
+    TODO: perform matrix multplication using
+        - multiprocessing
+        - multithreading
+        - GPU (figure out techniques?)
+    */
+    
+    return 0;
+}
